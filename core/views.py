a243fa3a3bef5fd51f07eb0695760c73ecfe9ce9
v-adhithyan <aavispeaks@gmail.com>
2018-12-22 10:26:45 +0530

    add poc

--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
 from django.http.response import HttpResponse
-
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 
 def hello_world(request):
     return HttpResponse("India 2k19 - Visualizations")
+
+
+class poc(TemplateView):
+    template_name = "poc.html"
