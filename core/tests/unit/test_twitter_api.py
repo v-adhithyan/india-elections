c11d461cafab834b4630e28b694ba5bc4726391c
v-adhithyan <aavispeaks@gmail.com>
2018-12-31 17:41:14 +0530

    fix travis test failing

--- core/tests/unit/test_twitter_api.py
+++ core/tests/unit/test_twitter_api.py
@@ -3,6 +3,10 @@ import mock
 from core.twitter.twitter_api import TwitterApi
 
 
+@mock.patch('core.constants.TW_CONSUMER_KEY', 'test')
+@mock.patch('core.constants.TW_CONSUMER_SECRET', 'test')
+@mock.patch('core.constants.TW_ACCESS_TOKEN', 'test')
+@mock.patch('core.constants.TW_ACCESS_TOKEN_SECRET', 'test')
 def test_get_and_save_tweets(tweets):
     with mock.patch(
         'core.twitter.twitter_api.TwitterApi.get_and_save_tweets'
