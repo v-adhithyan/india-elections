ecd97eb9cdab93e3222795e760fce758d25b307c
v-adhithyan <aavispeaks@gmail.com>
2018-12-31 07:47:02 +0530

    add tweet model

--- core/models.py
+++ core/models.py
@@ -1,1 +1,37 @@
-# Create your models here.
+from datetime import datetime
+
+from django.db import models
+
+from core.constants import SENTIMENTS
+
+
+class Tweet(models.Model):
+    q = models.CharField(max_length=50)
+    tweet = models.TextField()
+    cleaned_text = models.TextField()
+    sentiment = models.CharField(choices=SENTIMENTS, max_length=1)
+    added_time = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def save_to_db(cls, tweet_dict):
+        tweet = Tweet()
+        tweet.tweet = tweet_dict['dict']
+        tweet.cleaned_text = tweet_dict['cleaned_tweet']
+        tweet.sentiment = tweet_dict['tweet_sentiment']
+        tweet.save()
+
+    @classmethod
+    def bulk_save(cls, tweet_dicts):
+        for tweet_dict in tweet_dicts:
+            Tweet(
+                q=tweet_dict['q'],
+                tweet=tweet_dict['tweet'],
+                cleaned_text=tweet_dict['cleaned_tweet'],
+                sentiment=tweet_dict['tweet_sentiment'],
+                added_time=datetime.now()).save()
+
+    def __str__(self):
+        return "{} :: {}".format(self.sentiment, self.tweet)
+
+    def __unicode__(self):
+        return "{} :: {}".format(self.sentiment, self.tweet)
---
+++ core/twitter/utils.py
@@ -0,0 +1,18 @@
+import re
+
+import textblob
+from django.utils.encoding import smart_text
+
+
+def clean_tweet(tweet):
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", smart_text(tweet)).split())
+
+
+def get_tweet_sentiment(tweet) -> str:
+    analysis = textblob.TextBlob(clean_tweet(tweet))
+    if analysis.sentiment.polarity > 0:
+        return "positive"
+    elif analysis.sentiment.polarity == 0:
+        return "neutral"
+    else:
+        return "negative"
