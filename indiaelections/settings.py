7aac82b75f62df7a9f662596db126f74810743d1
v-adhithyan <aavispeaks@gmail.com>
2019-02-01 10:29:59 +0530

    Add static root

--- indiaelections/settings.py
+++ indiaelections/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static"),
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, "static"),
 )
