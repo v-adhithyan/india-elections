f461868540eac94999846329d97771282946be2c
v-adhithyan <aavispeaks@gmail.com>
2019-03-27 07:47:34 +0530

    fix job access permission
    (cherry picked from commit 8c25862775c5825fa3ace8eec0c3aa923a7b7fee)

--- core/job/permissions.py
+++ core/job/permissions.py
@@ -10,4 +10,10 @@ class JobAccess(BasePermission):
     def has_permission(self, request, view):
         host = request.META.get('HTTP_REFERER', None)
 
-        return host and host == LOCALHOST
+        is_allowed = [host and host == LOCALHOST]
+        if "-" in host and "-" in LOCALHOST:
+            host = host.split("-")[0]
+            localhost = LOCALHOST.split('-')[0]
+            is_allowed.append(host == localhost)
+
+        return any(is_allowed)
